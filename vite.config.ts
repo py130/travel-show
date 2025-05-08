2243c456f75a04492ce844480ae11bb888ad3b44
yuepang <pycmy@outlook.com>
2025-05-08 16:11:03 +0800

    弃用vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig(({ mode }) => {
   // 加载环境变量
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue()],
     server: {
       proxy: {
         [env.VITE_API_BASE_URL]: {
