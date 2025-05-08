4b0cd023e65618800ad47541019ac2d95e7a4df7
yuepang <pycmy@outlook.com>
2025-05-08 11:49:38 +0800

    移动端适配；邀请函demo

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,23 @@
 <script setup lang="ts">
 import { ref } from "vue";
 const apiResponse = ref("Loading...");
-import HelloWorld from "./components/HelloWorld.vue";
 import { onMounted } from "vue";
 
 onMounted(async () => {
-  const response = await fetch("/api/joke");
+  const response = await fetch("/api/amorous-poem");
   const data = await response.text();
   apiResponse.value = data;
 });
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <h1>Welcome to Our Wedding</h1>
+  <p>py & cmy</p>
   <div class="api-response">{{ apiResponse }}</div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.api-response {
+  margin-top: 20px;
 }
 </style>
